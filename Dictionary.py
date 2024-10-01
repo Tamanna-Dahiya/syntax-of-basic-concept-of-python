3d10de0d1fb71041559caff8b9504abf07ac5a1d
yashksaini-coder <ys3853428@gmail.com>
2024-10-01 20:55:05 +0530

    fix: 🐛 Check for errors & refractor the code
    All teh mistakes & error are pointed out at the end of each file
    
    ✅ Closes: #1

--- Dictionary.py
+++ Dictionary.py
@@ -1,28 +1,43 @@
-# Dictionary is a datatype that stores key value pair.it is storewd in curly brackets{}
-'''dictionary is mutable,unordered,indexed but can't contain duplicate keys'''
-d={"name":"tamanna","course":"btechcse","year":1}# if you give duplicate keys in a dictionary then you will get name error 
-#to access values by using keys
- print(d["name"])#if you committ some error in name of keys then you will get key error
-# to create an empty dictionary
-d={}
-# to add keys in empty dictionary
-d["name"]="Tamanna Dahiya "
-d["profession"]="student"
+# Dictionary is a datatype that stores key-value pairs. It is stored in curly brackets {}
+'''Dictionary is mutable, unordered, indexed but can't contain duplicate keys'''
+d = {"name": "tamanna", "course": "btechcse", "year": 1}  # if you give duplicate keys in a dictionary then you will get a name error
+
+# To access values by using keys
+print(d["name"])  # if you commit some error in the name of keys then you will get a key error
+
+# To create an empty dictionary
+d = {}
+
+# To add keys in an empty dictionary
+d["name"] = "Tamanna Dahiya"
+d["profession"] = "student"
 print(d)
-#Nested dictionary -in simple words nested dictionary means a dictionary within another dictionary
-d1={"a":{"b":2},"d":3}
-# you can also give list or tuple as a value in dictionary
-# one most important point-in a dictionary two values can be same but two keys cannot
-# now let's discuss some of the most important methods of dictionary
-#first is .keys()-it will give you all the keys present in a dictionary(keys will be given in list)
+
+# Nested dictionary - in simple words nested dictionary means a dictionary within another dictionary
+d1 = {"a": {"b": 2}, "d": 3}
+
+# You can also give list or tuple as a value in a dictionary
+# One most important point - in a dictionary two values can be the same but two keys cannot
+
+# Now let's discuss some of the most important methods of dictionary
+# .keys() - it will give you all the keys present in a dictionary (keys will be given in a list)
 print(d.keys())
-# .values()-it will give you all the values of dictionary( values will be given in list)
+
+# .values() - it will give you all the values of the dictionary (values will be given in a list)
 print(d.values())
-#.items()-ot will give combination of key and value pair in tuple
+
+# .items() - it will give a combination of key and value pair in a tuple
 print(d.items())
-#.get() -its work will be similar to this (print(d["name"])
-#but the benefit of using .get is if you give wrong name of key then it will give none 
-print(d.get("name")) 
-# last method which we will discuss is that .update()
-d.update({"me":"learner"})
+
+# .get() - its work will be similar to this (print(d["name"]))
+# but the benefit of using .get is if you give the wrong name of the key then it will give None
+print(d.get("name"))
+
+# .update() - it will update the dictionary with the given key-value pair
+d.update({"me": "learner"})
 print(d)
+
+# Mistakes and Refactoring:
+# 1. Fixed indentation and spacing issues.
+# 2. Corrected comments for better readability.
+# 3. Removed unnecessary comments and improved grammar.
--- tuples.py
+++ tuples.py
@@ -1,20 +1,27 @@
-# Tuple is an immutable data type 
-# it is enclosed by round brackets
-t=([1,2],3)
-# indexing as same concepts as we discussed in strings and list
+# Tuple is an immutable data type
+# It is enclosed by round brackets
+t = ([1, 2], 3)
+
+# Indexing as same concepts as we discussed in strings and lists
 print(t[0])
-t[0]=1#not possible to do this as tuple is immutable ,it will throw type error
-# wap to create an empty tuple
-t=()
-# to create a tuple of single element
-t=(1,)# comma after this 1 is necessary if this is not given and if you will fetch its type then it will come integer
-t=(2)
-print(type(t))# int
-# now let's discuss the methods of tuple
-#find()- it will give you the index of first occurence of an elemnet in tuple if elemnet does not exist then value error will come
-t1=(1,2,3,4,3,2)
-t1.find(2)
-print(t1)
-#count()-it will tell how many times an element comes in tuple if element does not exist then it will give 0
-t1.count(2)
-print(t1)
+# t[0] = 1  # not possible to do this as tuple is immutable, it will throw type error
+
+# WAP to create an empty tuple
+t = ()
+
+# To create a tuple of a single element
+t = (1,)  # comma after this 1 is necessary if this is not given and if you fetch its type then it will come as integer
+t = (2)
+print(type(t))  # int
+
+# Now let's discuss the methods of tuple
+# index() - it will give you the index of the first occurrence of an element in the tuple if the element does not exist then value error will come
+t1 = (1, 2, 3, 4, 3, 2)
+print(t1.index(2))
+
+# count() - it will tell how many times an element comes in the tuple if the element does not exist then it will give 0
+print(t1.count(2))
+
+# Mistakes and Refactoring:
+# 1. Fixed incorrect method name (find() to index()).
+# 2. Improved readability and consistency in comments.
