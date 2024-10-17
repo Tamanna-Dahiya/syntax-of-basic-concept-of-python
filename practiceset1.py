3e6f96ffad91408c3a5240d6aa01d2a98591fabf
Tamanna-Dahiya <tamannadahiya726@gmail.com>
2024-10-17 07:11:55 +0530

    Practice questions related to basic concepts

---
+++ practiceset1.py
@@ -0,0 +1,25 @@
+#program to add two numbers
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+sum=num1+num2
+print(sum)
+#program to find remainder when a number is divided by z
+num=int(input("enter a number:"))
+z=int(input("enter another number:"))
+print(num/z)
+#program to check the type of variable assigned using input function
+x=input("enter your name:")
+print(type(x))
+#program to check whether the value of variable b is greater than value a or not
+a=34
+b=80
+print(a>b)
+#program to find average of two numbers
+n=int(input("enter a number:"))
+m=int(input("enter second number:"))
+avg=(n+m)/2
+print(avg)
+#program to calculate square of number entered by user
+number=int(input("enter a number:"))
+square=number**2
+print(square)
