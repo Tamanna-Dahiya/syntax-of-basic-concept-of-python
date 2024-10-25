42e447b48b13cebd5e1b7a07e914058136a07dff
Tamanna-Dahiya <tamannadahiya726@gmail.com>
2024-10-25 08:32:27 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,24 @@
+# Practice questions to get clearity of concept I am learning.
+# First question- To print area and perimeter of circle
+ #r=float(input("enter radius:"))
+ #choice=int(input("enter number:"))
+#if choice==0:
+  #  print("area=",3.14*r**2)
+ #elif choice==1:
+  #  print("perimeter=",2*3.14*r)
+ #else:
+   # print("none")        
+#Second question-
+ #num=int(input("enter a number:"))
+ #num1=int(input("enter second number:"))
+ #if num%num1==0:
+  #  print(num,"is divisible by",num1) 
+ #else:
+  #  print(num,"is not divisible by",num1) 
+# Third question
+#to print table of number
+num=int(input("enter a number:"))
+for i in range(1,11):
+    print(num,"X",i,"=",num*i)
+#fourth question
+    
